768571c1e785ceb32bfad0cd3531c1216a6b8f6b
Niklaus Hirt <niklaushirt@gmail.com>
2020-05-05 15:50:43 +0200

    new k8smonitor registration

--- 9_register_k8_monitor.sh
+++ 9_register_k8_monitor.sh
@@ -46,30 +46,20 @@ echo "--------------------------------------------------------------------------
 
 
 
-        while getopts "d:n:f:" opt
+        while getopts "d:n:f:i:" opt
         do
           case "$opt" in
-              d ) INPUT_DOCKER_DOMAIN="$OPTARG" ;;
               f ) INPUT_CONFIG="$OPTARG" ;;
+              i ) INPUT_PPA="$OPTARG" ;;
+              d ) INPUT_PATH="$OPTARG" ;;
           esac
         done
-
-
-        if [[ $INPUT_DOCKER_DOMAIN == "" ]];
-        then
-            echo "    ${RED}ERROR${NC}: Please provide the Docker Hub Domain"
-            echo "    USAGE: $0 -d <DOCKER_DOMAIN> -f <CONFIGURATION_FILE>  "
-            exit 1
-        else
-          echo "    ${GREEN}Docker Hub Domain OK:${NC}                $INPUT_DOCKER_DOMAIN"
-          K8M_DOMAIN=$INPUT_DOCKER_DOMAIN
-        fi
 
 
         if [[ $INPUT_CONFIG == "" ]];
         then
             echo "    ${RED}ERROR${NC}: Please provide the ibm-cloud-apm-dc-configpack.tar file"
-            echo "    USAGE: $0 -d <DOCKER_DOMAIN> -f <CONFIGURATION_FILE>   "
+            echo "    USAGE: $0 -d <DOCKER_DOMAIN> -f <CONFIGURATION_FILE> -i <PPA_FILE>  "
             exit 1
         else
           echo "    ${GREEN}Config File OK:${NC}                      $INPUT_CONFIG"
@@ -77,6 +67,24 @@ echo "--------------------------------------------------------------------------
         fi
 
 
+        if [[ $INPUT_PPA == "" ]];
+        then
+            echo "    ${RED}ERROR${NC}: Please provide the PPA install file (like agent_ppa_2020.1.0_prod_amd64.tar.gz)"
+            echo "    USAGE: $0 -d <DOCKER_DOMAIN> -f <CONFIGURATION_FILE> -i <PPA_FILE>   "
+            exit 1
+        else
+          echo "    ${GREEN}PPA File OK:${NC}                         $INPUT_PPA"
+          PPA_CONFIG=$INPUT_PPA
+        fi
+
+
+        if [[ $INPUT_PATH == "" ]];
+        then
+          echo "    ${ORANGE}No Path provided, using${NC}             '$TEMP_PATH'"
+        else
+          echo "    ${GREEN}Path OK:${NC}                             '$INPUT_PATH'"
+          TEMP_PATH=$INPUT_PATH
+        fi
 
 
 echo "---------------------------------------------------------------------------------------------------------------------------"
@@ -113,6 +121,17 @@ echo "--------------------------------------------------------------------------
             exit 1
         fi
 
+
+        echo "    Check if ${CYAN}$PPA_CONFIG${NC} exists"
+        if test -f "$PPA_CONFIG"; then
+            echo "    ${GREEN}  OK${NC}"
+        else 
+            echo "    ${RED}  ERROR${NC}: ${ORANGE}ibm-cloud-apm-dc-configpack.tar${NC} does not exist in your Path"
+            echo "           ${RED}Aborting.${NC}"
+            exit 1
+        fi
+
+
 echo "---------------------------------------------------------------------------------------------------------------------------"
 echo "---------------------------------------------------------------------------------------------------------------------------"
 echo "  "
@@ -134,6 +153,10 @@ echo "--------------------------------------------------------------------------
 echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
 
         getClusterFQDN
+
+        getInstallPath
+
+        export my_registry_name="image-registry-openshift-image-registry."$CLUSTER_NAME
 
 echo "---------------------------------------------------------------------------------------------------------------------------"
 echo "---------------------------------------------------------------------------------------------------------------------------"
@@ -158,15 +181,15 @@ echo "--------------------------------------------------------------------------
 echo " ${BLUE}Your configuration${NC}"
 echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
 echo "    ${GREEN}CLUSTER TO BE REGISTERED :${NC}   $CLUSTER_NAME"
-echo "    ----------------------------------------------------------------------------------------------------------------------------------------------------"
-echo "    ${GREEN}IMPORT NAME :${NC}                $K8M_IMPORT_NAME"
-echo "    ----------------------------------------------------------------------------------------------------------------------------------------------------"
-echo "    ${GREEN}Docker Domain for images:${NC}    $K8M_DOMAIN"
-echo "    ${GREEN}Config File:${NC}                 $K8M_CONFIG"
+echo "    ------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "    ${GREEN}Docker Domain for images:${NC}    $my_registry_name"
+echo "    ------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "    ${GREEN}Configuration file:${NC}          $K8M_CONFIG"
+echo "    ${GREEN}PPA File:${NC}                    $PPA_CONFIG"
 echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
 echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
-echo "---------------------------------------------------------------------------------------------------------------------------"
-echo "---------------------------------------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
 echo "  "
 echo "  "
 echo "  "
@@ -206,10 +229,68 @@ echo " ${CYAN}Running Prerequisites${NC}"
 echo "---------------------------------------------------------------------------------------------------------------------------"
 echo "---------------------------------------------------------------------------------------------------------------------------"
 
+        export SCRIPT_PATH=$(pwd)
+
+        echo " ${wrench} Create Config Directory"
+          rm -r $INSTALL_PATH/* 
+          mkdir -p $INSTALL_PATH 
+        echo "    ${GREEN}  OK${NC}"
+        echo "  "
+
+
+        echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
+        echo " ${wrench} Copying ${CYAN}Files${NC}"
+        cp $PPA_CONFIG $INSTALL_PATH/agent_ppa_2020.1.0_prod_amd64.tar.gz
+        cp $K8M_CONFIG $INSTALL_PATH/ibm-cloud-apm-dc-configpack.tar
+        mkdir -p $INSTALL_PATH/deploy
+        cp -r ./tools/apm/deploy/* $INSTALL_PATH/deploy
+        cd $INSTALL_PATH
+
+
+        echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
+        echo " ${wrench} Loading ${CYAN}Images${NC} to Openshift Registry "
+
+        echo " ${wrench} Unpacking ${CYAN}Images${NC} "
+        tar xvf agent_ppa_2020.1.0_prod_amd64.tar.gz images/
+
+        echo " ${wrench} Adapt ${CYAN}Registry Route${NC} "
+        export local_registry=$(oc registry info)
+        oc create route reencrypt --service=image-registry
+        oc get route image-registry
+
+
+        echo " ${wrench} Docker ${CYAN}Login${NC} "
+        docker login -u $(oc whoami) -p $(oc whoami -t) "$my_registry_name"
+
+        echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
+        echo " ${wrench} Loading ${CYAN}Agentoperator Image${NC}"
+        docker load -i images/agentoperator_APM_202003100816.tar.gz
+        docker tag agentoperator:APM_202003100816 $my_registry_name/multicluster-endpoint/agentoperator:APM_202003100816  
+        docker push $my_registry_name/multicluster-endpoint/agentoperator:APM_202003100816
+
+        echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
+        echo " ${wrench} Loading ${CYAN}K9sMonitor Image${NC}"
+        docker load -i images/k8-monitor_APM_202003092352.tar.gz
+        docker tag k8-monitor:APM_202003092352 $my_registry_name/multicluster-endpoint/k8-monitor:APM_202003092352  
+        docker push $my_registry_name/multicluster-endpoint/k8-monitor:APM_202003092352
+       
+        echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
+        echo " ${wrench} Loading ${CYAN}k8sdc-Operator Image${NC}"
+        docker load -i images/k8sdc-operator_APM_202003092352.tar.gz
+        docker tag k8sdc-operator:APM_202003092352 $my_registry_name/multicluster-endpoint/k8sdc-operator:APM_202003092352  
+        docker push $my_registry_name/multicluster-endpoint/k8sdc-operator:APM_202003092352
+       
+        echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
+        echo " ${wrench} Loading ${CYAN}Reloader Image${NC}"
+        docker load -i images/reloader_202002170811-multi-arch.tar.gz
+        docker tag reloader:202002170811-multi-arch $my_registry_name/multicluster-endpoint/reloader:202002170811-multi-arch  
+        docker push $my_registry_name/multicluster-endpoint/reloader:202002170811-multi-arch
+
+
         echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
         echo " ${wrench} Unpack ${CYAN}Configpack${NC}"
-        cp $K8M_CONFIG ./tools/apm/ibm-cloud-apm-dc-configpack.tar 
-        tar -xvf ./tools/apm/ibm-cloud-apm-dc-configpack.tar --directory ./tools/apm/
+        cp $K8M_CONFIG .
+        tar -xvf ./ibm-cloud-apm-dc-configpack.tar
 
         echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
         echo " ${wrench} Create ${CYAN}Cluster Roles${NC}"
@@ -218,13 +299,25 @@ echo "--------------------------------------------------------------------------
 
         oc create clusterrolebinding icamklust-binding_default --clusterrole=cluster-admin \
           --serviceaccount=multicluster-endpoint:default -n multicluster-endpoint
+
         oc adm policy add-cluster-role-to-user cluster-admin IAM#nikh@ch.ibm.com --as=system:admin
 
         echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
         echo " ${wrench} Create ${CYAN}Secrets${NC}"
-        kubectl -n multicluster-endpoint create -f ./tools/apm/ibm-cloud-apm-dc-configpack/dc-secret.yaml
+        kubectl -n multicluster-endpoint create -f ./ibm-cloud-apm-dc-configpack/dc-secret.yaml
 
-        kubectl -n multicluster-endpoint create secret generic ibm-agent-https-secret --from-file=./tools/apm/ibm-cloud-apm-dc-configpack/keyfiles/cert.pem --from-file=./tools/apm/ibm-cloud-apm-dc-configpack/keyfiles/ca.pem --from-file=./tools/apm/ibm-cloud-apm-dc-configpack/keyfiles/key.pem
+        kubectl -n multicluster-endpoint create secret generic ibm-agent-https-secret --from-file=./ibm-cloud-apm-dc-configpack/keyfiles/cert.pem --from-file=./ibm-cloud-apm-dc-configpack/keyfiles/ca.pem --from-file=./ibm-cloud-apm-dc-configpack/keyfiles/key.pem
+
+
+        echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
+        echo " ${wrench} Adapting ${CYAN}YAML Files${NC}"
+
+        ${SED} -i "s/MY_REGISTRY/$local_registry/" deploy/agentoperator.yaml
+        ${SED} -i "s/MY_REGISTRY/$local_registry/" deploy/icam-reloader.yaml
+        ${SED} -i "s/MY_REGISTRY/$local_registry/" deploy/operator.yaml
+
+
+
 
 
 echo "---------------------------------------------------------------------------------------------------------------------------"
@@ -250,13 +343,13 @@ echo "--------------------------------------------------------------------------
 echo "---------------------------------------------------------------------------------------------------------------------------"
 
 
-        kubectl apply -n multicluster-endpoint -f ./tools/apm/deploy/crds/k8sdc_crd.yaml
-        kubectl apply -n multicluster-endpoint -f ./tools/apm/deploy/agentoperator.yaml
-        kubectl apply -n multicluster-endpoint -f ./tools/apm/deploy/icam-reloader.yaml
-        kubectl apply -n multicluster-endpoint -f ./tools/apm/deploy/operator.yaml
-        kubectl apply -n multicluster-endpoint -f ./tools/apm/deploy/role.yaml
-        kubectl apply -n multicluster-endpoint -f ./tools/apm/deploy/role_binding.yaml
-        kubectl apply -n multicluster-endpoint -f ./tools/apm/deploy/service_account.yaml
+        kubectl apply -n multicluster-endpoint -f ./deploy/crds/k8sdc_crd.yaml
+        kubectl apply -n multicluster-endpoint -f ./deploy/agentoperator.yaml
+        kubectl apply -n multicluster-endpoint -f ./deploy/icam-reloader.yaml
+        kubectl apply -n multicluster-endpoint -f ./deploy/operator.yaml
+        kubectl apply -n multicluster-endpoint -f ./deploy/role.yaml
+        kubectl apply -n multicluster-endpoint -f ./deploy/role_binding.yaml
+        kubectl apply -n multicluster-endpoint -f ./deploy/service_account.yaml
 
         waitForPodsReady "multicluster-endpoint"
 
